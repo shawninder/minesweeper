5baf04b36360b81acc1dc28ffda445d544e702b2
Shawn Inder <shawninder@gmail.com>
2026-03-11 15:33:54 -0500

    Basic gameboard and attempt at fixing deployment

--- components/Gameboard.tsx
+++ components/Gameboard.tsx
@@ -1,7 +1,14 @@
 export default function Gameboard() {
   return (
     <div>
-      <div className='w-96 h-96 bg-red-500'></div>
+      <div className='grid grid-cols-9 grid-rows-9 w-96 h-96'>
+        {Array.from({ length: 81 }).map((_, idx) => (
+          <button
+            key={idx}
+            className='border border-gray-400 bg-gray-100 w-full h-full aspect-square'
+          ></button>
+        ))}
+      </div>
     </div>
   )
 }
--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 
+const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? ''
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export'
+  output: 'export',
+  basePath: isProd ? `/${repo}` : '',
+  assetPrefix: isProd ? `/${repo}/` : ''
 };
 
 export default nextConfig;
