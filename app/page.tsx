fd20ad3579689095340b2b583da5a53b6cde1a2a
Shawn Inder <shawninder@gmail.com>
2026-03-11 14:29:02 -0500

    Small change to test deploymet again

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import Header from "@/components/Header"
+import Gameboard from "@/components/Gameboard"
 
 export default function Home() {
-  return <Header />
+  return (
+    <div className='flex flex-col items-center justify-center'>
+      <Header />
+      <Gameboard />
+    </div>
+  )
 }
---
+++ components/Gameboard.tsx
@@ -0,0 +1,7 @@
+export default function Gameboard() {
+  return (
+    <div>
+      <div className='w-96 h-96 bg-red-500'></div>
+    </div>
+  )
+}
