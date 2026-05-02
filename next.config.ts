1328af6cc5f842a7fc9c19c21960e47bbc3632ef
Shawn Inder <shawninder@gmail.com>
2026-05-02 12:39:39 -0400

    Update deps + fix audit

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,16 @@
 import type { NextConfig } from "next";
+import { ip } from "address"
 
 const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? ''
 const isProd = process.env.NODE_ENV === 'production'
-
+const localIp = ip()
+console.log({localIp})
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
   basePath: isProd && repo ? `/${repo}` : '',
-  assetPrefix: isProd && repo ? `/${repo}/` : ''
+  assetPrefix: isProd && repo ? `/${repo}/` : '',
+  allowedDevOrigins: [`${localIp}`, `${localIp}:3000`]
 };
 
 export default nextConfig;
