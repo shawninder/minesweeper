49afd830dea2c82143011f0a23e54607190d3436
Shawn Inder <shawninder@gmail.com>
2026-05-02 12:39:21 -0400

    Simple mine density

--- lib/gameLogic.ts
+++ lib/gameLogic.ts
@@ -249,13 +249,8 @@ export function evaluateGameState(
 }
 
 export function getMineCount(cellCount: number) {
-  const MINE_CURVE_A = 0.000467
-  const MINE_CURVE_B = -0.0576
-  const MINE_CURVE_C = 11.61
-
-  return Math.round(
-    MINE_CURVE_A * cellCount * cellCount + MINE_CURVE_B * cellCount + MINE_CURVE_C
-  )
+  const MINE_DENSITY = 0.20
+  return Math.floor(cellCount * MINE_DENSITY)
 }
 
 export type AvailableSpace = {
