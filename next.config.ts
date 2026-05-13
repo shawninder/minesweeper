213ec8f35195e7a2932c8ad1c15ccda52456e631
Shawn Inder <shawninder@gmail.com>
2026-05-13 19:47:09 -0400

    Enable service worker

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ import { ip } from "address"
 const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? ''
 const isProd = process.env.NODE_ENV === 'production'
 const localIp = ip()
-console.log({localIp})
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
